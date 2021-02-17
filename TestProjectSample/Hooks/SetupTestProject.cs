3d88bb9a1c722ffe44bf873399e0413048af4c5c
Andreas Willich <andreas@willich.io>
2021-02-17 14:50:22 +0100

    initial commit

---
+++ TestProjectSample/Hooks/SetupTestProject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using TestProject.OpenSDK.Drivers.Generic;
+
+namespace TestProjectSample.Hooks
+{
+    [Binding]
+    public class SetupTestProject
+    {
+        private GenericDriver _genericDriver;
+
+        [BeforeScenario()]
+        public void CreateDriver()
+        {
+            _genericDriver = new GenericDriver(projectName: "Webinar February 2021", jobName: "TestProjectSample");
+        }
+    }
+}
