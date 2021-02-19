9f0b493fef62987582cc313902e09efafb3abfe1
Andreas Willich <andreas@willich.io>
2021-02-19 16:22:37 +0100

    got it working

--- TestProjectSample/Hooks/SetupTestProject.cs
+++ TestProjectSample/Hooks/SetupTestProject.cs
@@ -9,12 +9,27 @@ namespace TestProjectSample.Hooks
     [Binding]
     public class SetupTestProject
     {
+        private readonly ScenarioContext _scenarioContext;
         private GenericDriver _genericDriver;
+
+        public SetupTestProject(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
 
         [BeforeScenario()]
         public void CreateDriver()
         {
             _genericDriver = new GenericDriver(projectName: "Webinar February 2021", jobName: "TestProjectSample");
+        }
+
+        [AfterScenario]
+        public void CloseDriver()
+        {
+
+            _genericDriver.Report().Test(_scenarioContext.ScenarioInfo.Title, _scenarioContext.TestError is null);
+
+            _genericDriver.Quit();
         }
     }
 }
